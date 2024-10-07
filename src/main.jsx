d59217a22f74522277a61a059fa1aa43bce0c6a5
Steph Elsley <161695703+Stephh-E@users.noreply.github.com>
2024-10-07 20:14:56 +1100

    Start of character creation

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { k } from "./kaboomCtx.jsx";
-import { displayDialogue, setCamScale } from "./utils";
+import { displayDialogue, setCamScale } from "./utils.jsx";
 
 
 createRoot(document.getElementById('root')).render(
@@ -14,9 +14,10 @@ createRoot(document.getElementById('root')).render(
 
 k.loadSprite("spritesheet", "./spritesheet.png"), {
   sliceX: 39,
-  sliceY: 
+  sliceY: 31,
   anims: {
     "idle-down": 862,
+    "walk-down": {from: 862, to: 863, loop: true, speed: 8}
 
   },
 }
