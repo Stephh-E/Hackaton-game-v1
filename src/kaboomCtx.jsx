814154969184ec8e27b3b27033b9ae9a13291db2
celestemarieb <34022835+celestemarieb@users.noreply.github.com>
2024-10-08 20:15:53 +1100

    removed some code from previous tutorial

--- src/kaboomCtx.jsx
+++
@@ -1,8 +0,0 @@
-import kaboom from "kaboom";
-
-export const k = kaboom({
-    global: false,
-    // translates all touch events to click events
-    touchToMouse: true,
-    canvas: document.getElementById("game"),
-});
