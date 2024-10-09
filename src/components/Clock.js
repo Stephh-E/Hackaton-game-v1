baa758f4cbec59f50cb511ba430475029e1c1f2e
celestemarieb <34022835+celestemarieb@users.noreply.github.com>
2024-10-09 19:06:03 +1100

    created clock component

---
+++ src/components/Clock.js
@@ -0,0 +1,18 @@
+// manages time 
+import React from 'react';
+import {useState, useEffect} from 'react';
+
+const Clock = () => {
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+// https://www.npmjs.com/package/react-countdown
+
+    return (
+        <div className="clock">
+
+        </div>
+    );
+};
+
+export default Clock
---
+++ src/components/GameOver.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Score from './Score.js'; 
+
+// use game context ; score, time, enemies, candles collected
+
+// score + time 
+// decides if game is over or not 
+// stops game play
+// returns  - dialog box? game over and button option to restart 
+//  const [gameOver, setGameOver] = useState(false); 
